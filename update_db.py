b86a26514c614efa47f010a9c163380ade4a11b2
mithrilwoodrat <mithrilwoodrat@gmail.com>
2015-04-26 23:08:11 +0800

    add update_db.py

---
+++ update_db.py
@@ -0,0 +1,13 @@
+from tombhub.database import init_db
+import sqlite3
+
+con = sqlite3.connect("tempdb.sqlite")
+c = con.cursor()
+def drop_table(table):
+    c.execute("drop table %s" %(table))
+    con.commit()
+    con.close()
+
+if __name__ == '__main__':
+    drop_table('threads')
+    init_db()
